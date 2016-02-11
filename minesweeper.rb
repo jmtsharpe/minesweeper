0a92701c99016b29b6c7a351503cbc08b7a97ef7
James & Jordan <jordanholliday@gmail.com>
2016-02-11 12:34:42 -0500

    create Board and Tile classes.

---
+++ minesweeper.rb
@@ -0,0 +1,39 @@
+class Board
+
+  def initialize(size = 9)
+    @grid = Array.new(size) { Array.new(size) {Tile.new} }
+  end
+
+  def lay_mines
+    total_mines = [false,false,false,true]
+    @grid.each do |row|
+      row.each do |col|
+        col
+
+
+  end
+
+end
+
+
+class Tile
+
+  def initialize
+    @mine = false
+    @revealed = false
+    @flagged = false
+  end
+
+  def make_mine
+    @mine = true
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  def flag
+    @flagged ? @flagged = false : @flagged = true
+  end
+
+end
