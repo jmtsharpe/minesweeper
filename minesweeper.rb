67b0db39d5aca3b5ab3c2931aa3cf4aa3d20dffc
James & Jordan <jordanholliday@gmail.com>
2016-02-11 14:13:14 -0500

    Add Game#adj_array method

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,7 @@
 class Board
 
+  attr_reader :grid
+
   def initialize(size = 9)
     @grid = Array.new(size) { Array.new(size) {Tile.new} }
   end
@@ -8,20 +10,16 @@ class Board
 
     @grid.each do |row|
       row.each do |col|
-        mine_or_not = rand(4)
-        col.make_mine if mine_or_not == 3
+        mine_or_not = rand(3)
+        col.make_mine if mine_or_not == 2
       end
     end
-
-
   end
 
 end
 
 
 class Tile
-
-
 
   def initialize
     @mine = false
@@ -39,6 +37,35 @@ class Tile
 
   def flag
     @flagged ? @flagged = false : @flagged = true
+  end
+
+end
+
+class Game
+
+  attr_reader :board
+
+  def initialize(size = 9)
+    @board = Board.new(size)
+  end
+
+  def adj_array(arr)
+    row, col = arr[0], arr[1]
+
+    rows = [row, row - 1, row + 1]
+    cols = [col, col - 1, col + 1]
+
+    size = board.grid.length - 1
+    adj_array = []
+    rows.each do |el|
+      if el >= 0 && el <= size
+        cols.each do |el2|
+          adj_array << board.grid[el][el2] if el2 >= 0 && el2 <= size
+        end
+      end
+    end
+
+    adj_array[1..-1]
   end
 
 end
